f44ceccdc98c76fe749112026af08264cebed753
Nankun-Xu <xu.nank@northeastern.edu>
2023-12-26 16:02:13 -0800

    Add products redux slices

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+// import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+// import cookieParser from 'cookie-parser';
+dotenv.config();
+import productRoutes from './routers/productRoutes.js';
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+})
+
+app.use('/api/products', productRoutes)
+// app.use('/api/users', userRoutes);
+// app.use('/api/orders', orderRoutes);
+// app.use('/api/upload', uploadRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+
+app.listen(PORT, () =>
+    console.log(`http://localhost:${PORT}`)
+);

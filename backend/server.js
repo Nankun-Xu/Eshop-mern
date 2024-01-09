c50932ade99f8d684a4356a1888be51fa30b26c7
Nankun-Xu <xu.nank@northeastern.edu>
2024-01-08 16:52:52 -0800

    checkout process to place order

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 dotenv.config();
 import productRoutes from './routers/productRoutes.js';
 import userRoutes from './routers/userRoutes.js';
+import orderRoutes from './routers/orderRoutes.js'
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
@@ -25,7 +26,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes);
-// app.use('/api/orders', orderRoutes);
+app.use('/api/orders', orderRoutes);
 // app.use('/api/upload', uploadRoutes);
 
 app.use(notFound);
